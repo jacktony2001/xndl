9858cb8830d0299b188477c46cb0accbe0a44ebb
jacktony2001 <halmsal@atomicmail.io>
2026-07-19 20:59:04 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+TELEGRAM_BOT_TOKEN = "8859818805:AAErwN7F71rgsuCozyussQWKS6ZFIazCWnY"
+TELEGRAM_CHAT_ID = "8956194322"
+BASE_URL = "https://www.aznude.com"
+START_PAGE = "https://www.aznude.com/browse/videos/recent/1.html"
